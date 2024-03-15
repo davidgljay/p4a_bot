9790f5adf62638f67eb38c7f9ae288e88facc0e4
David Jay <davidgljay@gmail.com>
2024-03-14 19:33:14 -0700

    Adding basic gcal event handler functions wtih appropriate tests.

--- functions/gcal_event_handler.js
+++ functions/gcal_event_handler.js
@@ -1,8 +1,8 @@
-const NotionWrapper = require('../apis/notion');
+const NotionWrapper = require('./apis/notion');
 
 // Expects an object with the following:
 // {
-//  "id": "Event ID",
+//  "id": "Event Gcal ID",
 //   "summary": "Event summary",
 //   "location": "Event location",
 //   "description": "Event description",
@@ -15,30 +15,44 @@ const NotionWrapper = require('../apis/notion');
 //   "duration": "Event duration"
 // }
 
+const eventsDatabaseId = 'YOUR_EVENTS_DATABASE_ID'; // Replace with your actual database ID
+const registrationsDatabaseId = 'YOUR_REGISTRATIONS_DATABASE_ID'; // Replace with your actual database ID
+const contactsDatabaseId = 'YOUR_CONTACTS_DATABASE_ID'; // Replace with your actual database ID
+
 // Function to check and create/update event in "events" database
 async function handleEventUpdate(event) {
-    const eventsDatabaseId = 'YOUR_EVENTS_DATABASE_ID'; // Replace with your actual database ID
+
+    const filter = {
+            property: 'gcalid',
+            text: {
+                equals: event.id
+            }
+    }
 
     // Check if event already exists in "events" database
-    const existingEvent = await NotionWrapper.getEvent(eventsDatabaseId, event.id);
+    const results = await NotionWrapper.query(eventsDatabaseId, filter);
 
-    if (existingEvent) {
+    if (results.length > 0) {
+        const existingEvent = results[0];
         // Update existing event
-        await NotionWrapper.updateEvent(existingEvent.id, {
+        // TODO: handle matching with notion field IDs
+        await NotionWrapper.update(existingEvent.id, {
             summary: event.summary,
             description: event.description,
             link: event.link,
             duration: event.duration,
+            location: event.location,
             start: event.start,
             end: event.end
         });
     } else {
         // Create new event
-        await NotionWrapper.createEvent(eventsDatabaseId, {
+        await NotionWrapper.create(eventsDatabaseId, {
             id: event.id,
             summary: event.summary,
             description: event.description,
             link: event.link,
+            location: event.location,
             duration: event.duration,
             start: event.start,
             end: event.end
@@ -47,52 +61,94 @@ async function handleEventUpdate(event) {
 }
 
 // Function to check and create attendees in "contacts" database
-async function handleAttendees(event) {
-    const contactsDatabaseId = 'YOUR_CONTACTS_DATABASE_ID'; // Replace with your actual database ID
+// This is probably not necessary, as we can just create contacts as we create registrations.
+// async function handleAttendees(event) {
+//     const contactsDatabaseId = 'YOUR_CONTACTS_DATABASE_ID'; // Replace with your actual database ID
 
-    for (let i = 0; i < event.attendee_emails.length; i++) {
-        const email = event.attendee_emails[i];
-        const name = event.attendee_names[i];
+//     for (let i = 0; i < event.attendee_emails.length; i++) {
+//         const email = event.attendee_emails[i];
+//         const name = event.attendee_names[i];
 
-        // Check if attendee already exists in "contacts" database
-        const existingAttendee = await NotionWrapper.getAttendee(contactsDatabaseId, email);
+//         const filter = {
+//             property: 'email',
+//             text: {
+//                 equals: email
+//             }
+//         } //TODO: Is there a way to do this more efficiently through a big or filter?
 
-        if (!existingAttendee) {
-            // Create new attendee
-            await NotionWrapper.createAttendee(contactsDatabaseId, {
-                email,
-                name
-            });
+//         // Check if attendee already exists in "contacts" database
+//         const results = await NotionWrapper.query(contactsDatabaseId, filter);
+
+//         if (results.length === 0) {
+//             // Create new attendee
+//             await NotionWrapper.create(contactsDatabaseId, {
+//                 email,
+//                 name
+//             });
+//         }
+//         // TODO: Handle case where we don't yet have someone's name, though this seems edge right now.
+//     }
+// }
+
+
+
+// Function to check and create/update registrations in "registrations" database
+async function handleRegistration(event) {
+
+    const filter = {
+            property: 'event',
+            text: {
+                equals: event.id
+            }
         }
-    }
-}
-
-// Function to check and create/update registration in "registrations" database
-async function handleRegistration(user, event) {
-    const registrationsDatabaseId = 'YOUR_REGISTRATIONS_DATABASE_ID'; // Replace with your actual database ID
 
     // Check if registration already exists in "registrations" database
-    const existingRegistration = await NotionWrapper.getRegistration(registrationsDatabaseId, user, event.id);
+    const results = await NotionWrapper.query(registrationsDatabaseId, filter);
 
-    if (existingRegistration) {
-        // Update existing registration
-        await NotionWrapper.updateRegistration(existingRegistration.id, {
-            user,
-            event: event.id,
-            response: event.attendee_responses[user]
-        });
-    } else {
-        // Create new registration
-        await NotionWrapper.createRegistration(registrationsDatabaseId, {
-            user,
-            event: event.id,
-            response: event.attendee_responses[user]
-        });
+    let registeredEmails = new Set();
+    for (let i = 0; i < results.length; i++) {
+        const existingRegistration = results[i];
+        for (let j = 0; j < event.attendee_emails.length; j++) {
+            const email = event.attendee_emails[j];
+            if (existingRegistration.email == email) {
+                registeredEmails.add(email);
+                if (existingRegistration.response !== event.attendee_responses[j]) {
+                    await NotionWrapper.update(existingRegistration.id, {
+                        response: event.attendee_responses[j]
+                    });
+                }
+            }
+        }
+    }
+    
+    // Add registrations that aren't yet in the DB. 
+    for (let i = 0; i < event.attendee_emails.length; i++) {
+        const email = event.attendee_emails[i];
+        if (!registeredEmails.has(email)) {
+            // Check if contact already exists in "contacts" database, create it if it doesn't
+            const filter = {
+                property: 'email',
+                text: {
+                    equals: email
+                }
+            };
+            const contact = {
+                email,
+                name: event.attendee_names[i]
+            };
+            const contactRecord = await NotionWrapper.findOrCreate(contactsDatabaseId, filter, contact);
+            // Create new registration
+            await NotionWrapper.create(registrationsDatabaseId, {
+                user: contactRecord.id,
+                event: event.id,
+                response: event.attendee_responses[i]
+            });
+        }
+
     }
 }
 
 module.exports = {
     handleEventUpdate,
-    handleAttendees,
     handleRegistration
 };
---
+++ tests/gcal_event_handler.test.js
@@ -0,0 +1,188 @@
+const NotionWrapper = require("../functions/apis/notion");
+const {
+  handleEventUpdate,
+  handleRegistration,
+} = require("../functions/gcal_event_handler");
+
+describe("handleEventUpdate", () => {
+  const eventsDatabaseId = "YOUR_EVENTS_DATABASE_ID";
+  const event = {
+    id: "Event Gcal ID",
+    summary: "Event summary",
+    location: "Event location",
+    description: "Event description",
+    start: "Event start time",
+    end: "Event end time",
+    attendee_emails: ["email1", "email2"],
+    attendee_names: ["name1", "name2"],
+    attendee_responses: ["response1", "response2"],
+    link: "Event link",
+    duration: "Event duration",
+  };
+
+  beforeEach(() => {
+    NotionWrapper.query = jest.fn();
+    NotionWrapper.update = jest.fn();
+    NotionWrapper.create = jest.fn();
+  });
+
+  it("should call update function if NotionWrapper.query returns a result", async () => {
+    const existingEvent = { id: "existingEventId" };
+    NotionWrapper.query.mockResolvedValue([existingEvent]);
+
+    await handleEventUpdate(event);
+
+    expect(NotionWrapper.query).toHaveBeenCalledWith(
+      eventsDatabaseId,
+      expect.objectContaining({
+        property: "gcalid",
+        text: {
+          equals: event.id,
+        },
+      })
+    );
+    expect(NotionWrapper.update).toHaveBeenCalledWith(existingEvent.id, {
+      summary: event.summary,
+      description: event.description,
+      link: event.link,
+      duration: event.duration,
+      location: event.location,
+      start: event.start,
+      end: event.end,
+    });
+    expect(NotionWrapper.create).not.toHaveBeenCalled();
+  });
+
+  it("should call create function if NotionWrapper.query returns no results", async () => {
+    NotionWrapper.query.mockResolvedValue([]);
+
+    await handleEventUpdate(event);
+
+    expect(NotionWrapper.query).toHaveBeenCalledWith(
+      eventsDatabaseId,
+      expect.objectContaining({
+        property: "gcalid",
+        text: {
+          equals: event.id,
+        },
+      })
+    );
+    expect(NotionWrapper.create).toHaveBeenCalledWith(eventsDatabaseId, {
+      id: event.id,
+      summary: event.summary,
+      description: event.description,
+      link: event.link,
+      location: event.location,
+      duration: event.duration,
+      start: event.start,
+      end: event.end,
+    });
+    expect(NotionWrapper.update).not.toHaveBeenCalled();
+  });
+});
+
+describe("handleRegistration", () => {
+  const registrationsDatabaseId = "YOUR_REGISTRATIONS_DATABASE_ID";
+  const contactsDatabaseId  = "YOUR_CONTACTS_DATABASE_ID";
+
+  var event;
+
+  beforeEach(() => {
+    event = {
+      id: "Event Gcal ID",
+      attendee_emails: ["email1", "email2"],
+      attendee_names: ["name1", "name2"],
+      attendee_responses: ["response1", "response2"],
+    };
+  
+    NotionWrapper.query = jest.fn();
+    NotionWrapper.update = jest.fn();
+    NotionWrapper.create = jest.fn();
+    NotionWrapper.findOrCreate = jest.fn();
+  });
+
+  it("should not create or update if registration already exists and the response is the same", async () => {
+    const existingRegistrations = [{ id: "existingRegistrationId" , email: "email1", response: "response1" }, { id: "existingRegistrationId2" , email: "email2", response: "response2" }];
+    NotionWrapper.query.mockResolvedValueOnce(existingRegistrations);
+
+    await handleRegistration(event);
+
+    expect(NotionWrapper.query).toHaveBeenCalledWith(
+      registrationsDatabaseId,
+      expect.objectContaining({
+        property: "event",
+        text: {
+          equals: event.id,
+        },
+      })
+    );
+    expect(NotionWrapper.update).not.toHaveBeenCalled();
+    expect(NotionWrapper.create).not.toHaveBeenCalled();
+    expect(NotionWrapper.findOrCreate).not.toHaveBeenCalled();
+  });
+
+  it("should update if registration exists with different response", async () => {
+    const existingRegistrations = [
+      { id: "existingRegistrationId", email: "email1", response: "oldResponse"},
+      { id: "existingRegistrationId2" , email: "email2", response: "response2" }
+    ];
+    NotionWrapper.query.mockResolvedValue(existingRegistrations);
+
+    await handleRegistration(event);
+
+    expect(NotionWrapper.query).toHaveBeenCalledWith(
+      registrationsDatabaseId,
+      expect.objectContaining({
+        property: "event",
+        text: {
+          equals: event.id,
+        },
+      })
+    );
+    expect(NotionWrapper.update).toHaveBeenCalledWith(
+      existingRegistrations[0].id,
+      {
+        response: event.attendee_responses[0],
+      }
+    );
+    expect(NotionWrapper.create).not.toHaveBeenCalled();
+    expect(NotionWrapper.findOrCreate).not.toHaveBeenCalled();
+  });
+
+  it("should find or create a contact and create a new registration if one doesn't exist", async () => {
+    const newContact = { id: "newContactId" };
+    NotionWrapper.query.mockResolvedValue([]);
+    NotionWrapper.findOrCreate.mockResolvedValue(newContact);
+
+    await handleRegistration(event);
+
+    expect(NotionWrapper.query).toHaveBeenCalledWith(
+      registrationsDatabaseId,
+      expect.objectContaining({
+        property: "event",
+        text: {
+          equals: event.id,
+        },
+      })
+    );
+    expect(NotionWrapper.update).not.toHaveBeenCalled();
+    expect(NotionWrapper.create).toHaveBeenCalledWith(registrationsDatabaseId, {
+      user: newContact.id,
+      event: event.id,
+      response: event.attendee_responses[0],
+    });
+    expect(NotionWrapper.findOrCreate).toHaveBeenCalledWith(
+      contactsDatabaseId,
+      expect.objectContaining({
+        property: "email",
+        text: {
+          equals: event.attendee_emails[0],
+        },
+      }),
+      {
+        email: event.attendee_emails[0],
+        name: event.attendee_names[0],
+      }
+    );
+  });
+});
