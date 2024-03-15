79a63a093c150e6a021694355304e5ab5c2fa514
David Jay <davidgljay@gmail.com>
2024-03-14 19:48:08 -0700

    Adding simple gcloud function.

---
+++ functions/gcal_event.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const {  handleEventUpdate, handleRegistration } = require('./handlers/gcal_event_handler');
+
+exports.gcal_event = functions.https.onRequest((req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(400).send('Invalid request method');
+    }
+
+    const authToken = req.headers['authorization'];
+    const expectedToken = 'Bearer MY_SECRET'
+
+    if (authToken !== expectedToken) {
+        return res.status(403).send('Unauthorized');
+    }
+
+    const event = req.body;
+    handleEventUpdate(event);
+    handleRegistration(event);
+
+    res.status(200).send('Success');
+});
