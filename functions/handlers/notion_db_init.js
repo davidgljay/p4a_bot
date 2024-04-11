27d56819ff24e0f50e4853cb53e3dfd8baa28339
David Jay <davidgljay@gmail.com>
2024-04-10 21:07:34 -0700

    Addressing bigs in notion_db_init

--- functions/handlers/notion_db_init.js
+++ functions/handlers/notion_db_init.js
@@ -1,84 +1,182 @@
-const notionWrapper = require('/apis/notion.js');
+const NotionWrapper = require('../apis/notion.js');
+const functions = require('firebase-functions');
+const notionClient = new NotionWrapper(functions.config().notion.token);
 
 function initializeNotion(parentPageId) {
     const contactsSchema = {
-        title: 'Contacts',
-        properties: {
             email: {
                 type: 'email',
+                email: {}
             },
             name: {
                 type: 'title',
+                title: {}
             },
             dietary_restrictions: {
                 type: 'multi_select',
-                options: [
-                    { name: 'Vegetarian' },
-                    { name: 'Vegan' },
-                    { name: 'Gluten-free' },
-                    { name: 'Kosher' },
-                    { name: 'Halal' },
-                ],
+                multi_select: {
+                    options: [
+                        { name: 'Vegetarian' },
+                        { name: 'Vegan' },
+                        { name: 'Gluten-free' },
+                        { name: 'Kosher' },
+                        { name: 'Halal' },
+                    ],
+                },
+                    
             },
             allergies: {
-                type: 'text'
+                type: 'rich_text',
+                rich_text: {}
             },
             location: {
-                type: 'text',
+                type: 'rich_text',
+                rich_text: {}
             },
-        },
     };
 
     const eventsSchema = {
-        title: 'Events',
-        properties: {
+            title: {
+                type: 'title',
+                title: {}
+            },
             time: {
                 type: 'date',
+                date: {}
             },
             location: {
-                type: 'text',
-            },
-            host: {
-                type: 'relation',
-                collection_id: 'contacts_database_id',
-                property_name: 'name',
-            },
-        },
+                type: 'rich_text',
+                rich_text: {}
+            }
+    };
+
+    const eventsRelations = {
+        host: {
+            type: 'relation',
+            collection_id: 'contacts_database_id',
+            property_name: 'name',
+        }
     };
 
     const registrationsSchema = {
-        title: 'Registrations',
-        properties: {
-            event: {
-                type: 'relation',
-                collection_id: 'events_database_id',
-                property_name: 'title',
-            },
-            contact: {
-                type: 'relation',
-                collection_id: 'contacts_database_id',
-                property_name: 'name',
+            title: {
+                type: 'title',
+                title: {}
             },
             status: {
                 type: 'select',
-                options: [
+                select: {
+                    options: [
                     { name: 'Confirmed' },
                     { name: 'Registered' },
                     { name: 'Confirmed'},
                     { name: 'Attended' },
                     { name: 'Regrets' },
                 ],
-            },
-        },
+            }
+            }
     };
 
-    const contactDB = notionWrapper.createDatabase(parentPageId, contactsSchema);
-    const eventsDB = notionWrapper.createDatabase(parentPageId, eventsSchema);
-    const registrationDB = notionWrapper.createDatabase(parentPageId, registrationsSchema);
+    const registrationsRelations = {
+        event: {
+            type: 'relation',
+            relation: {
+                database_id: 'events_database_id',
+            }
+        },
+        contact: {
+            type: 'relation',
+            collection_id: 'contacts_database_id',
+            property_name: 'name',
+        }
+    };
 
-    return Promise.all([contactDB, eventsDB, registrationDB]).then((databases) => {
-        const [contacts, events, registrations] = databases;
-        return { contacts, events, registrations };
+    const chaptersSchema = {
+            charter: {
+                type: 'url',
+                url: {},
+            },
+            name: {
+                type: 'title',
+                title: {}
+            }
+    };
+
+    const chaptersRelations = {
+        leads: {
+            type: 'relation',
+            relation: {
+                database_id: 'contacts_database_id',
+            }
+        },
+        cohosts: {
+            type: 'relation',
+            relation: {
+                database_id: 'contacts_database_id',
+            }
+        },
+        dinners: {
+            type: 'relation',
+            collection_id: 'events_database_id',
+            property_name: 'title',
+        },
+        impact: {
+            type: 'relation',
+            collection_id: 'impact_reports_database_id',
+            property_name: 'title',
+        }
+    }
+
+    const impactReportsSchema = {
+            summary: {
+                type: 'title',
+                title: {}
+            },
+            created_on: {
+                type: "created_time",
+                created_time: {}
+            },
+            tags: {
+                type: 'multi_select',
+                multi_select: {
+                    options: [
+                    { name: 'Funding' },
+                    { name: 'Conversation' },
+                    { name: 'Support' },
+                    ],
+                }
+            }
+    };
+
+    const impactReportsRelations = {
+        created_by: {
+            type: 'relation',
+            collection_id: 'contacts_database_id',
+            property_name: 'name',
+        }
+    };
+
+
+    const chaptersDB = notionClient.createDatabase(parentPageId, 'Chapters', chaptersSchema);
+    const impactReportsDB = notionClient.createDatabase(parentPageId, 'Impact Reports', impactReportsSchema);
+    const contactDB = notionClient.createDatabase(parentPageId, 'Contacts', contactsSchema);
+    const eventsDB = notionClient.createDatabase(parentPageId, 'Events', eventsSchema);
+    const registrationDB = notionClient.createDatabase(parentPageId, 'Registrations', registrationsSchema);
+
+    const promiseArray = [contactDB, eventsDB, registrationDB, chaptersDB, impactReportsDB]
+    var delay = 0
+    const increment = 5000
+    const delayedPromiseArray = []
+    for (promise in promiseArray) {
+        var delayedPromise = new Promise(resolve => setTimeout(resolve, delay)).then(promise)
+        delay += increment
+        console.log(delay)
+        delayedPromiseArray.push(delayedPromise)
+    }
+
+    return Promise.all(delayedPromiseArray).then((databases) => {
+        const [contacts, events, registrations, chapters, impactReports] = databases;
+        return { contacts, events, registrations, chapters, impactReports };
     })
 }
 
--- functions/index.js
+++ functions/index.js
@@ -10,12 +10,12 @@
 const functions = require('firebase-functions');
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-const expectedToken = `Bearer ${functions.config.CUSTOM_AUTH_TOKEN}`;
+const expectedToken = `Bearer ${functions.config().custom.auth_token}`;
 
 const {
   handleEventUpdate,
   handleRegistration} = require("./handlers/gcal_event_handler");
-const {initializeNotion} = require("./handlers/notion_db_init");
+const initializeNotion = require("./handlers/notion_db_init");
 const { on } = require('supertest/lib/test');
 
 exports.gcal_event = onRequest((req, res) => {
@@ -55,7 +55,11 @@ exports.initialize_notion = onRequest((req, res) => {
 
   if (authToken !== expectedToken) {
     logger.error("Unauthorized request", {structuredData: true});
-    return res.status(403).send("Unauthorized");
+    return res.status(403).send("Unauthorized, received tojen: " + authToken);
+  }
+  if (!req.body.parentPageId) {
+    logger.error("No parent page ID provided", {structuredData: true});
+    return res.status(400).send("No parent page ID provided");
   }
   const parentPageId = req.body.parentPageId;
 
@@ -68,7 +72,7 @@ exports.initialize_notion = onRequest((req, res) => {
     logger.error("Error initializing Notion", error);
     return res.status(500).send("Error initializing Notion");
   }
-}
+});
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
