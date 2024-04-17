3a5af74f82b5e1894795170c1f56c51b7946b6dc
David Jay <davidgljay@gmail.com>
2024-04-17 15:00:18 -0700

    Adding function to sync local runtimeconfig and remote config.

---
+++ functions/syncconfig.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+// Read the .runtimeconfig.json file
+const config = JSON.parse(fs.readFileSync('.runtimeconfig.json', 'utf8'));
+
+// Function to set a config variable
+function setConfig(path, value) {
+  const command = `firebase functions:config:set ${path}="${value}"`;
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      return;
+    }
+    console.log(`Stdout: ${stdout}`);
+  });
+}
+
+// Function to traverse the config object
+function traverse(obj, path = '') {
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      // If the value is an object, recurse
+      traverse(obj[key], `${path}${key}.`);
+    } else {
+      // If the value is not an object, set the config variable
+      setConfig(`${path}${key}`, obj[key]);
+    }
+  }
+}
+
+// Start the traversal
+traverse(config);
