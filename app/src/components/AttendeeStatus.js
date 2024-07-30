ef6e8fba72e2e7076318414a3e1c4886dec8b12c
David Jay <davidgljay@gmail.com>
2024-07-29 21:47:55 -0700

    Updating style of AttendeeStatus to better hanle mobile.

--- app/src/components/AttendeeStatus.js
+++ app/src/components/AttendeeStatus.js
@@ -4,15 +4,15 @@ import Button from '@mui/material/Button';
 const AttendeeStatus = ({ status, setStatus }) => {
     return (
         <div>
-            <p>You are currently listed as:</p>
+            <div style={styles.header}>You are currently listed as:</div>
             <div style={styles.buttonContainer}>
-                <Button style={styles.button} variant={status=='Accepted' ? 'contained' : 'outlined'} color="success" onClick={() => setStatus('Accepted')}>
+                <Button sx={styles.button} variant={status=='Accepted' ? 'contained' : 'outlined'} color="success" onClick={() => setStatus('Accepted')}>
                     Attending
                 </Button>
-                <Button style={styles.button} variant={status=='Maybe' ? 'contained' : 'outlined'} color="warning" onClick={() => setStatus('Maybe')}>
+                <Button sx={styles.button} variant={status=='Maybe' ? 'contained' : 'outlined'} color="warning" onClick={() => setStatus('Maybe')}>
                     Maybe
                 </Button>
-                <Button style={styles.button} variant={status=='Declined' ? 'contained' : 'outlined'} color="error" onClick={() => setStatus('Declined')}>
+                <Button sx={styles.button} variant={status=='Declined' ? 'contained' : 'outlined'} color="error" onClick={() => setStatus('Declined')}>
                     Regrets
                 </Button>
             </div>
@@ -22,13 +22,23 @@ const AttendeeStatus = ({ status, setStatus }) => {
 };
 
 const styles = {
+    header: {
+        fontSize: '1rem',
+        textAlign: 'center',
+        margin: '.5rem',
+        marginTop: '1rem',
+    },
     buttonContainer: {
         display: 'flex',
         justifyContent: 'center',
     },
     button: {
         margin: '0.5rem',
-        width: '1200px',
+        fontSize: '1rem',
+        maxWidth: '400px',
+        '@media (max-width: 600px)': {
+            fontSize: '0.5rem',
+        }
     }
 };
 
