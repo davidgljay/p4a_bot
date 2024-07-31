fac8bf5c8c2b71d5936c7acb821ad2f86e2f7f5e
David Jay <davidgljay@gmail.com>
2024-07-31 16:01:38 -0700

    Making App a proper React component to utilize lifecycle methods.

--- app/src/App.js
+++ app/src/App.js
@@ -1,59 +1,61 @@
 import PotluckForm from "./components/PotluckForm";
 
-this.state = {
-  fname: null,
-  start_time: null,
-  address: null,
-  status: null,
-  dietaryRequirements: null,
-  dishSignups: [],
-  userDishSignup: null,
-  groupDietReqs: [],
-  numGuests: null
-}
+class App extends React.Component {
+  state = {
+    fname: null,
+    start_time: null,
+    address: null,
+    status: null,
+    dietaryRequirements: null,
+    dishSignups: [],
+    userDishSignup: null,
+    groupDietReqs: [],
+    numGuests: null
+  };
 
-componentDidMount() {
-  fetch('http://localhost:3001/potluck')
+  componentDidMount() {
+    fetch('http://localhost:3001/potluck')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          fname: data.fname,
+          start_time: data.start_time,
+          address: data.address,
+          status: data.status,
+          dietaryRequirements: data.dietary_requirements,
+          dishSignups: data.dish_signups,
+          groupDietReqs: data.group_dietary_requirements,
+          numGuests: data.num_guests
+        });
+      });
+    };
+
+  uploadForm = (userDishSignup, dietReqs, dishText, status) => {
+    fetch('http://localhost:3001/potluck', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        userDishSignup,
+        dietReqs,
+        dishText,
+        status
+      })
+    })
     .then(response => response.json())
     .then(data => {
-      this.setState({
-        fname: data.fname,
-        start_time: data.start_time,
-        address: data.address,
-        status: data.status,
-        dietaryRequirements: data.dietary_requirements,
-        dishSignups: data.dish_signups,
-        groupDietReqs: data.group_dietary_requirements,
-        numGuests: data.num_guests
-      });
+      console.log(data);
     });
-}
+  };
 
-uploadForm = (userDishSignup, dietReqs, dishText, status) => {
-  fetch('http://localhost:3001/potluck', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      userDishSignup,
-      dietReqs,
-      dishText,
-      status
-    })
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  });
-} 
-
-function App() {
-  return (
-    <div className="App">
-      <PotluckForm {...this.state} uploadForm={uploadForm}/>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <PotluckForm {...this.state} uploadForm={uploadForm}/>
+      </div>
+    );
+};
+};
 
 export default App;
