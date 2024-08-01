0d4e62ecd77de10f9a7963c42cc170965fbdadbb
David Jay <davidgljay@gmail.com>
2024-07-31 22:11:05 -0700

    Updating get_registration to use findObjectById in NotionClient.

--- functions/handlers/get_registration.js
+++ functions/handlers/get_registration.js
@@ -24,15 +24,6 @@ const { event } = require("firebase-functions/v1/analytics");
 const NotionWrapper = require("../apis/notion.js");
 const clientConfig = require('../config/client_config.js');
 
-// Function to iterate through object keys and check condition
-function findObjectById(obj, id) {
-    for (let key in obj) {
-        if (obj[key].id === id) {
-            return obj[key];
-        }
-    }
-}
-
 // Function to lookup object in "registrations" database
 async function lookupRegistration(id, client_org) {
     try {
@@ -44,7 +35,7 @@ async function lookupRegistration(id, client_org) {
         // TODO: Implement as a Promise.all to improve responsiveness
         const registration = await notionClient.get(id);
         const properties = registration.properties;
-        const event_id = findObjectById(properties, config.registrationFields.event).relation[0].id;
+        const event_id = notionClient.findObjectById(properties, config.registrationFields.event).relation[0].id;
 
         // Get all registrations for the event
         const event_filter = {
@@ -58,7 +49,7 @@ async function lookupRegistration(id, client_org) {
         for (let i = 0; i < event_registrations_raw.length; i++) {
             const event_registration = event_registrations_raw[i];
             event_registrations.push({
-                status: findObjectById(event_registration.properties, config.registrationFields.status).select.name,
+                status: notionClient.findObjectById(event_registration.properties, config.registrationFields.status).select.name,
                 name: event_registration.properties['Name'].formula.string,
                 dish_text: event_registration.properties['Dish'].rich_text.length > 0 ? event_registration.properties['Dish'].rich_text[0].text.content : null,
                 dish_type: event_registration.properties['Dish Type'].select ? event_registration.properties['Dish Type'].select.name : null,
@@ -77,11 +68,11 @@ async function lookupRegistration(id, client_org) {
         // TODO: Update database schema to include all fields so that fields can be called by ID rather than by name.
         const result = {
             id: registration.id,
-            status: findObjectById(properties, config.registrationFields.status).select.name,
+            status: notionClient.findObjectById(properties, config.registrationFields.status).select.name,
             fname: properties['Name'] && properties['Name'].formula.string.split(' ')[0],
             event_start: properties['Event Start Time'].rollup.array[0].date.start,
             event_address: properties['Event Location'].rollup.array.length > 0 ? properties['Event Location'].rollup.array[0].rich_text[0].text.content : null,
-            user_contact_id: findObjectById(properties,config.registrationFields.contact).relation[0].id,
+            user_contact_id: notionClient.findObjectById(properties,config.registrationFields.contact).relation[0].id,
             user_diet_req: properties['Dietary Requirements'].rollup.array.length > 0 ? properties['Dietary Requirements'].rollup.array[0].rich_text[0].text.content : null,
             user_dish_text: properties['Dish'].rich_text.length > 0 ? properties['Dish'].rich_text[0].text.content : null,
             user_dish_type: properties['Dish Type'].select ? properties['Dish Type'].select.name : null,
